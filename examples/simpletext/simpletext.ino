20c6faf137921e00188b287ac58a0fceb3323e1b
ToXIc <ToXIc-Dev@users.noreply.github.com>
2019-09-04 22:07:38 +0100

    Update simpletext.ino
    Made it so display is only redrawn if message has changed (Means it doesnt flicker constantly), also added code to change message and brightness from serial monitor

--- examples/simpletext/simpletext.ino
+++ examples/simpletext/simpletext.ino
@@ -9,9 +9,16 @@
 // If you're using the Scroll pHAT HD
 Adafruit_IS31FL3731_ScrPhHD matrix = Adafruit_IS31FL3731_ScrPhHD();
 
+// Type a message in the serial monitor to update the display
+// Type br + a number between 0-255 to update the brightness
+
+int Brightness = 1;
+String message = "BOB";
+String message2;
+String a;
+
 void setup() {
-  Serial.begin(9600);
-  Serial.println("ISSI manual animation test");
+  Serial.begin(115200);
   if (! matrix.begin()) {
     Serial.println("IS31 not found");
     while (1);
@@ -20,12 +27,31 @@ void setup() {
 }
 
 void loop() {
+
+while(Serial.available()) {
+a = Serial.readString();// read the incoming data as string
+if (a.indexOf("br") >=0 ) {
+ a.remove(0,2);
+ String b = a;
+ Brightness = b.toInt();
+ message2 = "";
+ Serial.println("Brightness set to: " + b);
+} else {
+message = a;
+Serial.println("Message set to: " + message);
+}
+}
+
+  if (message2 == message) {
+    
+  } else {
+  message2 = message;
   matrix.setRotation(0);
   matrix.setTextSize(1);
   matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
-  matrix.setTextColor(1);
+  matrix.setTextColor(Brightness);
   matrix.clear();
   matrix.setCursor(0,0);
-  matrix.print("BOB");
-  delay(100);
+  matrix.print(message2);
+  }
 }
